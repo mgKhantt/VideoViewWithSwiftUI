3ac2a324dcd6e319cc8f6884d2d26f21e1b6ad8b
mgKhantt <khantphonenaing156@gamil.com>
2024-11-04 21:21:10 +0630

    Finish

--- SwiftUI-Videos/ContentView.swift
+++ SwiftUI-Videos/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var videos:[Video] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(videos) { video in
+                HStack {
+                    Image(video.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90)
+                        .cornerRadius(8)
+                    
+                    
+                    Spacer().frame(width: 5)
+                    
+                    VStack(alignment: .leading) {
+                        Text(video.title)
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.75)
+                        Spacer()
+                            .frame(height: 10)
+                        Text(video.uploadDate)
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }.navigationTitle("Sean's Videos Part3")
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(videos: videoList.topTwelve)
 }
